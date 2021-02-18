4c99436a4ecc59c03dce79b9878e4df95cc38d0e
Tamar Al-Tahan <tamaraltahan@gmail.com>
2021-02-18 04:10:31 -0800

    Init

---
+++ Assets/Scripts/BaseProjectile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProjectile : MonoBehaviour
+{
+    Rigidbody2D projectile;
+    public float projSpeed;
+    public int damage;
+
+    private void Awake()
+    {
+        projectile = GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {
+        //Projectile will move forward from the point of origin
+        projectile.AddForce(Vector2.up * projSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Break the object if it hits a wall;
+        if (collision.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+        //destroys enemy it hits, then destroys the projectile
+        if (collision.CompareTag("Enemy"))
+        {
+            Destroy(collision.GetComponent<Rigidbody2D>());
+            Destroy(gameObject);
+        }
+        //if it hits a boss weakspot
+        if (collision.CompareTag("BossWeakPoint"))
+        {
+            //will enable this when enemy controller script is live with a takeDamage(int n) functions
+            //collision.GetComponent<EnemyController>().takeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}

70a617cbbbddf2847378dec7aac1d27946e7c32a
Tamar Al-Tahan <tamaraltahan@gmail.com>
2021-02-26 05:01:15 -0800

    Groundwork For weapon Switching
    experimenting with scriptable objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D bod;
+    public float lerpSpeed;
+    public float attackTimer = 0.25f;
+    float attackCD;
+    public WeaponSO weapon;
+    List<WeaponSO> weapons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bod = GetComponent<Rigidbody2D>();
+        weapons = new List<WeaponSO>(); //initialize a list of weapons
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //face the mouse cursor
+        Vector2 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * lerpSpeed);
+
+
+
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            if (scroll > 0) scrollWeaponUp();
+            else
+                scrollWeaponDown();
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && attackCD <= 0)
+        {
+            shoot();
+        }
+        //decrement the attack cooldown
+        if (attackCD > 0) attackCD -= Time.deltaTime;
+
+    }
+
+    void scrollWeaponUp()
+    {
+
+    }
+    void scrollWeaponDown()
+    {
+
+    }
+
+    void selectWeapon()
+    {
+
+    }
+
+
+    void shoot()
+    {
+        //Instantiate()
+        attackCD = attackTimer;
+    }
+}

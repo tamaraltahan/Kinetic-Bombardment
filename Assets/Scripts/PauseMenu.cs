3107f82154a086e23d1461a5bd7a11ac09bcc720
Braulio Amezquita <62687094+Slyhedgekirby@users.noreply.github.com>
2021-02-28 22:03:32 -0800

    Pause Menu now functions
    The pause menu now actually pauses the game and can be unpaused. The Pause menu now appears with a fade in when the game is paused instead of always being active.  The buttons for returning to the level select and main menu now send debug messages but that is all they do for now.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    //This is here so it can be used in other scripts since its public. 
+    //If another scipt uses it, it will be affected by the pasuing and unpausing if implemented to do so
+    public static bool GameIsPaused = false;
+
+    //Makes it so the pasue menu can actually be affected when the right gameobject is placed back in Unity.
+    public GameObject pauseMenuUI;
+    
+    //Resumes gameplay. Is public so it can be accesed by the button UI.
+     public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    } 
+    //Used to pasue the game. Keep public to use with buttons
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    //Used to switch to the menu but rightnow it only sends a debug message. Need to add the ability to switch scenes later.
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+    //Will be used to go to the level select. For now it just sends a debug message
+    public void LevelSelect()
+    {
+        Debug.Log("Loading level select...");
+    }
+
+}

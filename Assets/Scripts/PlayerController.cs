65713448a353a955d91e974c8e1204505c92d27e
Tamar Al-Tahan <tamaraltahan@gmail.com>
2021-03-01 04:47:23 -0800

    skeleton for weapon switching
    need help :(

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,21 +9,39 @@ public class PlayerController : MonoBehaviour
     public float attackTimer = 0.25f;
     float attackCD;
 
+
+    //experimenting with weapon switching
+    //using this as a template, but not verbatim, since its missing some features like ammo, which we will have to set on a level to level basis.
+    //https://answers.unity.com/questions/1775103/2d-weapon-switching-1.html
     weaponType activeWeapon;
-    public List<int> maxAmmoList;
-    List<int> currentAmmoList;
+    public List<int> maxAmmoList = new List<int>();
+    List<int> currentAmmoList = new List<int>();
+
+    List<weaponType> weaponsList = new List<weaponType>(); //maybe will do active weapons vs all weapons (?)
+
+    GameObject weaponObj; //this needs to be set somehow :/
+
+    
     public enum weaponType
     {
         standard,
         bounce
     }
+    //
 
     // Start is called before the first frame update
     void Start()
     {
+        bod = GetComponent<Rigidbody2D>(); // set the active body
+    }
 
-        bod = GetComponent<Rigidbody2D>();
-
+    void setAmmo()
+    {
+        //for each weapon in the list, set current ammo to max ammo
+        for (int i = 0; i < maxAmmoList.Count; ++i)
+        {
+            currentAmmoList[i] = maxAmmoList[i];
+        }
     }
 
     // Update is called once per frame
@@ -36,7 +54,8 @@ public class PlayerController : MonoBehaviour
 
 
 
-
+        //intent here is to be able to change weapon with scroll wheel
+        //not very necessary but why not
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         if (scroll != 0)
         {
@@ -48,13 +67,14 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) && attackCD <= 0)
         {
-            shoot();
+            shoot(weaponObj);
         }
         //decrement the attack cooldown
         if (attackCD > 0) attackCD -= Time.deltaTime;
 
     }
 
+    //to do
     void scrollWeaponUp()
     {
 
@@ -64,15 +84,25 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void selectWeapon()
+    void shoot(GameObject pewpew)
     {
-
+        Instantiate(pewpew, transform.position, Quaternion.identity);
+        attackCD = attackTimer;
     }
 
-
-    void shoot()
+    void selectWeapon(weaponType weapon)
     {
-        //Instantiate()
-        attackCD = attackTimer;
+        switch (weapon)
+        {
+            case weaponType.standard:
+                activeWeapon = weaponType.standard;
+                //weaponObj = something
+                break;
+            case weaponType.bounce:
+                activeWeapon = weaponType.bounce;
+                break;
+            default:
+                break;
+        }
     }
 }

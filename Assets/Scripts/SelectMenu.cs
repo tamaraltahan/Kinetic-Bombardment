d5c2bba6a433f4c0c9eb90dc0cddcf5e97fd0590
Justin Messina <jaycm98@gmail.com>
2021-05-03 13:10:53 -0700

    Beginning of save and load feature, working

--- Assets/Scripts/SelectMenu.cs
+++ Assets/Scripts/SelectMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SelectMenu : MonoBehaviour
 {
+    public int unlocked;
+
+    void start()
+    {
+        unlocked = PlayerPrefs.GetInt("unlocked", 1);
+    }
+    
     public void SelectLevel1()
     {
         SceneManager.LoadScene("Level1");
@@ -12,22 +19,35 @@ public class SelectMenu : MonoBehaviour
 
     public void SelectLevel2()
     {
-        SceneManager.LoadScene("Level2");
+        if (unlocked >= 2)
+        {
+            SceneManager.LoadScene("Level2");
+        }
+        
     }
 
     public void SelectLevel3()
     {
-        SceneManager.LoadScene("Level3");
+        if (unlocked >= 3)
+        {
+            SceneManager.LoadScene("Level3");
+        }
     }
 
     public void SelectLevel4()
     {
-        SceneManager.LoadScene("Level4");
+        if (unlocked >= 4)
+        {
+            SceneManager.LoadScene("Level4");
+        }
     }
 
     public void SelectLevel5()
     {
-        SceneManager.LoadScene("Level5");
+        if (unlocked >= 5)
+        {
+            SceneManager.LoadScene("Level5");
+        }
     }
 
     public void BackButton()
